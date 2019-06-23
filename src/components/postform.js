4a9daa3508ee2adc2042dd89f50af8ab8ffc0fb6
Mugenx <sahawu.mail@gmail.com>
2019-06-23 18:30:19 -0400

    REDUX

--- src/components/postform.js
+++ src/components/postform.js
@@ -2,31 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createPost } from '../actions/postAction';
 class PostForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      body: ''
-    };
+  state = {
+    title: '',
+    body: ''
+  };
 
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onChange(e) {
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-  onSubmit(e) {
+  onSubmit = e => {
     e.preventDefault();
-
     const post = {
       title: this.state.title,
       body: this.state.body
     };
-
     this.props.createPost(post);
-  }
+  };
   render() {
     return (
       <div>
