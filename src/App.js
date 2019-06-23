07f9f4832a475f0832d1c79b81d5d0a2ce8abd08
Mugenx <sahawu.mail@gmail.com>
2019-06-23 17:59:31 -0400

    REDUX

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-
 import Posts from './components/posts';
 import PostForm from './components/postform';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
+    <Provider store={store}>
       <div className="App">
         <PostForm />
         <hr />
         <Posts />
       </div>
+    </Provider>
   );
 }
 
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postAction';
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
     }
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(posts => this.setState({posts}))
-  }
-
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -30,4 +29,12 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+  newPost: state.posts.item
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(Posts);
